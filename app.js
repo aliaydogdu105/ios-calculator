5226d0374bb71eac9f4035ff1fd6c719a54ea448
aliaydogdu105 <aliaydogdu105@gmail.com>
2022-06-11 11:26:23 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*  1) first print the keypad to the screen
+    2) blocking spaces when pressed outside the keypad
+    3) prevent concatition when printing numbers
+    4) The event of throwing to the 2nd screen when 4 operations are pressed
+    5) math oparation on the 2nd screen
+    6) AC clear event
+    7) Equals function
+    8) Check if it prints from the keyboard */
+
+// operators can be given a single class
